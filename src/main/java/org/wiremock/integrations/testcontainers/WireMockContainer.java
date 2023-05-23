65ff4780954dc3ff20f637de1082659d43e56dd1
Julian Akkaya <julian.akkaya@axelspringer.com>
2023-05-23 11:56:31 +0200

    Do static import instead of wildcard

--- src/main/java/org/wiremock/integrations/testcontainers/WireMockContainer.java
+++ src/main/java/org/wiremock/integrations/testcontainers/WireMockContainer.java
@@ -28,7 +28,12 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
