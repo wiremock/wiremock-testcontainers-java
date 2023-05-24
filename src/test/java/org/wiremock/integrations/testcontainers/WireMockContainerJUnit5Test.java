5051fe9738998fc84d76e77b9158ae8559dbb9d8
Julian Michelmann <45766492+julian-michelmann@users.noreply.github.com>
2023-05-24 10:11:18 +0200

    Disable WireMock banner in the CLI output by default  (#42)
    * Disable banner by default when container is not running verbose
    
    * Fix format
    
    Co-authored-by: Oleg Nenashev <o.v.nenashev@gmail.com>
    
    * Do static import instead of wildcard
    
    * add method with banner instead of showing the banner when starting verbose
    
    * Add separate unit test class
    
    * Remove mockito dependency
    
    * Add a method "withBanner" to enable the banner
    
    ---------
    
    Co-authored-by: Julian Akkaya <julian.akkaya@axelspringer.com>
    Co-authored-by: Oleg Nenashev <o.v.nenashev@gmail.com>

--- src/test/java/org/wiremock/integrations/testcontainers/WireMockContainerJUnit5Test.java
+++ src/test/java/org/wiremock/integrations/testcontainers/WireMockContainerJUnit5Test.java
@@ -1,8 +1,5 @@
 package org.wiremock.integrations.testcontainers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.net.http.HttpResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,46 +7,50 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.wiremock.integrations.testcontainers.testsupport.http.TestHttpClient;
 
+import java.net.http.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Testcontainers
 public class WireMockContainerJUnit5Test {
 
-  @Container
-  public WireMockContainer wiremockServer = new WireMockContainer("2.35.0")
-      .withMapping("hello", WireMockContainerTest.class, "hello-world.json")
-      .withMapping("hello-resource", WireMockContainerTest.class, "hello-world-resource.json")
-      .withFileFromResource("hello-world-resource-response.xml", WireMockContainerTest.class,
-          "hello-world-resource-response.xml");
+    @Container
+    public WireMockContainer wiremockServer = new WireMockContainer("2.35.0")
+            .withMapping("hello", WireMockContainerTest.class, "hello-world.json")
+            .withMapping("hello-resource", WireMockContainerTest.class, "hello-world-resource.json")
+            .withFileFromResource("hello-world-resource-response.xml", WireMockContainerTest.class,
+                    "hello-world-resource-response.xml");
 
 
-  @ParameterizedTest
-  @ValueSource(strings = {
-          "hello",
-          "/hello"
-  })
-  public void helloWorld(String path) throws Exception {
-    // given
-    String url = wiremockServer.getUrl(path);
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "hello",
+            "/hello"
+    })
+    public void helloWorld(String path) throws Exception {
+        // given
+        String url = wiremockServer.getUrl(path);
 
-    // when
-    HttpResponse<String> response = TestHttpClient.newInstance().get(url);
+        // when
+        HttpResponse<String> response = TestHttpClient.newInstance().get(url);
 
-    // then
-    assertThat(response.body())
-        .as("Wrong response body")
-        .contains("Hello, world!");
-  }
+        // then
+        assertThat(response.body())
+                .as("Wrong response body")
+                .contains("Hello, world!");
+    }
 
-  @Test
-  public void helloWorldFromFile() throws Exception {
-    // given
-    String url = wiremockServer.getUrl("/hello-from-file");
+    @Test
+    public void helloWorldFromFile() throws Exception {
+        // given
+        String url = wiremockServer.getUrl("/hello-from-file");
 
-    // when
-    HttpResponse<String> response = TestHttpClient.newInstance().get(url);
+        // when
+        HttpResponse<String> response = TestHttpClient.newInstance().get(url);
 
-    // then
-    assertThat(response.body())
-        .as("Wrong response body")
-        .contains("Hello, world!");
-  }
+        // then
+        assertThat(response.body())
+                .as("Wrong response body")
+                .contains("Hello, world!");
+    }
 }
